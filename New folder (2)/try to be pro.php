66ec9c06c125fc36633f8e1060ca6d52233dc25c
XxRv <67992272+XxRv@users.noreply.github.com>
2020-07-08 11:10:37 +0300

    Add files via upload

---
+++ New folder (2)/try to be pro.php
@@ -0,0 +1,104 @@
+<html>
+
+<link rel="icon"href="icon.gif">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body 
+
+{
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+<title>بدك تصير غبي؟</title>
+<style>
+    body{
+background-color: dimgray	
+}
+
+
+</style>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">home</a>
+<a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+
+
+<center>                             <!--تغيير لون خلفية الكتابة-->
+<p style="color:white;"><b><h1 style="background-color:green; border:5px solid gray">كيف تصير غبي في خمس ثواني</h1></b></p>
+<h1>من الامثلة على الغباء</h1>
+<img src="https://j.top4top.io/p_1650zvvyo1.png" height="300" width="300">
+<img src="https://c.top4top.io/p_165068khy1.jpg" height="300" width="300">
+
+<hr>
+<!--تنقل بين المواقع-->
+<b><h><a href="xxxrvxxx.php">Another sites</a></h></b>
+</center>   
+<center>
+<h1>كل ما عليك انك تتابع
+</h1>
+<h1>1-Bts</h1>
+<h1>
+2-black pink</h1>
+</center>
+ <br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<audio src="4Directed%20By%20Robert%20B%20Weide%20Remix.mp4" controls></audio>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
+
